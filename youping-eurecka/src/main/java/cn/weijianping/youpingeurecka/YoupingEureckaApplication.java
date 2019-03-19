ffb6b40b40469de951a8f5c08eebd20dc4882879
yuexia <yue_xia@aliyun.com>
2019-03-19 17:17:47 +0800

    Initial commit

---
+++ youping-eurecka/src/main/java/cn/weijianping/youpingeurecka/YoupingEureckaApplication.java
@@ -0,0 +1,13 @@
+package cn.weijianping.youpingeurecka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoupingEureckaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoupingEureckaApplication.class, args);
+    }
+
+}
