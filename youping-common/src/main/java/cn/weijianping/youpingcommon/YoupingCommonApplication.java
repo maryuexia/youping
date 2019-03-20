0274984b64b6306f394b8d9b0050410df43064c9
yuexia <yue_xia@aliyun.com>
2019-03-20 09:11:17 +0800

    common

---
+++ youping-common/src/main/java/cn/weijianping/youpingcommon/YoupingCommonApplication.java
@@ -0,0 +1,13 @@
+package cn.weijianping.youpingcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoupingCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoupingCommonApplication.class, args);
+    }
+
+}
